007929a39104db08ee6e73ef74d5a3d591172abf
zengjunfeng <zengjunfeng9981@163.com>
2021-03-16 09:53:15 +0800

    upd

--- rmi-server/src/rmi/server/RmiServer.java
+++ rmi-server/src/rmi/server/RmiServer.java
@@ -3,17 +3,21 @@ package rmi.server;
 import rmi.api.service.UserService;
 import rmi.server.service.UserServiceImpl;
 
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RmiServer {
-    public static void main(String[] args) throws RemoteException {
-        System.setProperty("java.rmi.server.hostname", "192.168.1.93");
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
+        //System.setProperty("java.rmi.server.hostname", "192.168.1.93");
         UserService userService = new UserServiceImpl();
         //服务器注册在本地
         Registry registry = LocateRegistry.createRegistry(1099);
-        registry.rebind("userService",userService);
+        //registry.rebind("userService", userService);
+        Naming.bind("//localhost:1099/userService", userService);
         System.out.println("rmi服务已启动");
     }
 }
